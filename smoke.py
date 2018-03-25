18f61fed8f3474a95716557f83ce45f2e69ceef7
Mike Rehberg <Coder@TR182.com>
2018-03-25 08:27:34 -0600

    Recalibrated level readings again. Reduced sleep time while booting. Updated booting text.

--- smoke.py
+++ smoke.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 #/home/pi/temp/smoke.py
 
-#    SMOKING Pi Zero   Version: 1.0
+#    SMOKING Pi Zero   Version: 1.1
 
 # Python program to read level guage in smoke oil tank and display to an ePaper
 # display from Papirus.
 
 # Input is from an Automation hat. 0-4 volts.
 
+# V 1.0
 # Recalibrated voltage to level reading.
 # Added a deadband for level change before updating display.
 
+# V 1.1
+# Recalibrated again.
+# Reduced sleep time.
+# Changed booting text.
 
 import automationhat
 import time
 
-time.sleep(10.0)  # Wait for the pi to finish booting.
+time.sleep(7.0)  # Wait for the pi to finish booting.
 
 from papirus import PapirusTextPos
 
@@ -28,7 +33,7 @@ text.Clear()
 time.sleep(1.0)
 text.AddText("N221TM", 10 ,0, 40, Id="Line-1")
 text.AddText("SMOKE TANK", 0, 35, 30, Id="lINE-2")
-text.AddText("_EMPTY_", 0, 60, 40, Id="Line-3")
+text.AddText("BOOTING", 0, 60, 40, Id="Line-3")
 text.WriteAll()
 time.sleep(2.0)
 
@@ -52,13 +57,14 @@ while True:
         text.UpdateText("Line-3", "<-OFF->")        # Smoke OFF
         text.WriteAll()
         time.sleep(2.0)
-    if value > 2.80: Level = "_FULL_"
-    if value < 2.81: Level = " 3/4"
-    if value < 1.30: Level = " 1/2"
-    if value < 0.91: Level = " 1/4"
-    if value < 0.30: Level = "-EMPTY-"
+    if value > 1.50: Level = "_FULL_"
+    if value < 1.20: Level = " 3/4"
+    if value < 0.80: Level = " 1/2"
+    if value < 0.40: Level = " 1/4"
+    if value < 0.20: Level = "-EMPTY-"
     print (Level)
     text.UpdateText("Line-3", Level)
+    text.UpdateText("Line-1", Level3)
     text.WriteAll()
     
     input2 = automationhat.input.two.read()
