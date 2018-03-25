f702818aad66cbf92467eb421b22acffe875b4b1
Mike Rehberg <Coder@TR182.com>
2018-03-25 08:23:56 -0600

    Recalibraged readings and added deadband of level change before updating display.

--- smoke.py
+++ smoke.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 #/home/pi/temp/smoke.py
 
-#    SMOKING Pi Zero    Version: 0.00
+#    SMOKING Pi Zero   Version: 1.0
 
 # Python program to read level guage in smoke oil tank and display to an ePaper
 # display from Papirus.
 
 # Input is from an Automation hat. 0-4 volts.
+
+# Recalibrated voltage to level reading.
+# Added a deadband for level change before updating display.
 
 
 import automationhat
@@ -25,28 +28,57 @@ text.Clear()
 time.sleep(1.0)
 text.AddText("N221TM", 10 ,0, 40, Id="Line-1")
 text.AddText("SMOKE TANK", 0, 35, 30, Id="lINE-2")
-text.AddText("EMPTY", 20, 60, 40, Id="Line-3")
+text.AddText("_EMPTY_", 0, 60, 40, Id="Line-3")
 text.WriteAll()
 time.sleep(2.0)
 
 while True:
     
     value= automationhat.analog.one.read()
-#    print(value)
-    Level2 = "{:.3f}".format(value)
-#    print(Level2)
-    if value < 4.0: Level = "1/2"
-    if value < 2.0: Level = "1/4"
-    if value < 1.0: Level = "EMPTY"
-    if value > 4.0: Level = "FULL"
-#    print (Level)
-    text.UpdateText("Line-3", Level2)
-    input2 = automationhat.input.two.read()
-#    print(input2)
-    text.WriteAll()
-    if automationhat.input.two.read():  # Quit when input-2 goes high
-        break
-    time.sleep(5.0)
+    print(value)
+    Level2 = "{:.2f}".format(value)
+    print(Level2)
 
+	
+    Level3 = Level2 + " V"
+
+    print (Level3)
+	
+    if automationhat.input.three.read():        # Smoke is ON
+        text.UpdateText("Line-3", "<@@@@@>")
+        text.WriteAll()
+        while automationhat.input.three.read():     # Snmoke ON, so Wait
+            time.sleep(1)
+        text.UpdateText("Line-3", "<-OFF->")        # Smoke OFF
+        text.WriteAll()
+        time.sleep(2.0)
+    if value > 2.80: Level = "_FULL_"
+    if value < 2.81: Level = " 3/4"
+    if value < 1.30: Level = " 1/2"
+    if value < 0.91: Level = " 1/4"
+    if value < 0.30: Level = "-EMPTY-"
+    print (Level)
+    text.UpdateText("Line-3", Level)
+    text.WriteAll()
+    
+    input2 = automationhat.input.two.read()
+    print("Input2=", input2)
+
+    if automationhat.input.two.read(): break # Quit when input-2 goes high
+        
+    newValue = automationhat.analog.one.read()
+    change = abs(newValue - value)
+    print("change ", change)
+    
+    while change < 0.08:
+        time.sleep(1.0)         # Sit here if input isn't changing
+        print("level not changing")
+        newValue = automationhat.analog.one.read()
+        change = abs(newValue - value)
+        if automationhat.input.two.read(): break   # Quit when input-2 goes high
+        if automationhat.input.three.read(): break # Smoke is ON
+    print("change ", change)
+    time.sleep(1.0)
+    
 exit()
 
