dc7cd726bfc78e93312724cea6f3c617044fd89c
Mike Rehberg <Coder@TR182.com>
2018-04-30 10:07:48 -0600

    Major clean up of OLED code.

--- smoke.py
+++ smoke.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #/home/pi/temp/smoke.py
 
-#					SMOKING Pi Zero   Version: 2.3.2
+#					SMOKING Pi Zero   Version: 2.3.ePaper.1
 
 # Python program to read level guage in smoke oil tank and display to an ePaper
 # display from Papirus.
@@ -42,61 +42,30 @@
 # Version without OLED display.
 # V 2.3.2 found more "disp.", ".draw", and "height" lines to comment out.
 
+# V2.3.ePaper.1
+# updated version standard
 
 
-print("Hi from Smoke.py")
+
+print("PaPiRus ePaper Smoke.py")
+print(" V 2.3.ePaper.1 without OLED")
 
 import automationhat
 import time
 import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
+###import Adafruit_SSD1306
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+###from PIL import Image
+###from PIL import ImageDraw
+###from PIL import ImageFont
 
 import subprocess
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-
-### time.sleep(7.0)  # Papirus needs to wait for the pi to finish booting.
+ time.sleep(7.0)  # Papirus needs to wait for the pi to finish booting.
 from papirus import PapirusTextPos
 
 
-# 128x32 OLED display with hardware I2C:
-##disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# Initialize OLED Display library.
-##disp.begin()
-
-# Clear OLED display.
-##disp.clear()
-##disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-##width = disp.width
-##height = disp.height
-##image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-##draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-##draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-##bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-font = ImageFont.truetype('/home/pi/temp/upheavtt.ttf' , 30)
-
-
-time.sleep(0.1)  # short pause after ads1015 class creation
 
 text = PapirusTextPos(False)
 
@@ -107,21 +76,6 @@ text.AddText("SMOKE TANK", 0, 35, 30, Id="lINE-2")
 text.AddText("BOOTING", 0, 60, 40, Id="Line-3")
 text.WriteAll()
 
-# Draw a black filled box to clear the image.
-##draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-#	cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
-#	CPU = subprocess.check_output(cmd, shell = True )
-
-OLED = "  N221TM"
-
-# Write two lines of text.
-
-##draw.text((x, top+4),	str(OLED), font=font, fill=255)
-
-# OLED Display image.
-##disp.image(image)
-##disp.display()
 time.sleep(2.0)
 
 
@@ -154,22 +108,10 @@ while True:
 	if automationhat.input.three.read():        # Smoke is ON
 		text.UpdateText("Line-3", "<@@@@@>")
 		text.WriteAll()
-		OLED = "@@@@@@"
-# Draw a black filled box to clear the image.
-##		draw.rectangle((0,0,width,height), outline=0, fill=0)
-##		draw.text((x, top+4),     str(OLED), font=font, fill=255)
-##		disp.image(image)	
-##		disp.display()
 		while automationhat.input.three.read():     # Snmoke ON, so Wait
 			time.sleep(1)
 		text.UpdateText("Line-3", "<-OFF->")        # Smoke OFF
 		text.WriteAll()
-		OLED = "<-OFF->"
-# Draw a black filled box to clear the image.
-##		draw.rectangle((0,0,width,height), outline=0, fill=0)
-##		draw.text((x, top+4),     str(OLED), font=font, fill=255)
-##		disp.image(image)
-##		disp.display()
 		time.sleep(2.0)
 	if value > 3.20: Level = "-FULL-"
 	if value < 3.21: Level = " 3/4"
@@ -184,11 +126,6 @@ while True:
 	text.UpdateText("Line-3", Level)
 	text.UpdateText("Line-1", Level3)
 	text.WriteAll()
-# Draw a black filled box to clear the image.
-##	draw.rectangle((0,0,width,height), outline=0, fill=0)
-##	draw.text((x, top+4),     str(gallonsF), font=font, fill=255)
-##	disp.image(image)
-##	disp.display()
 	
 	input2 = automationhat.input.two.read()
 	print("Input2=", input2)
